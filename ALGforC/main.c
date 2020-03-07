66ede4596a76e15f59fecc97f6eadeb9f8018475
jmme <jmme@foxmail.com>
2020-03-07 11:35:51 +0800

    Initial Commit

---
+++ ALGforC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ALGforC
+//
+//  Created by Guagua on 2020/3/7.
+//  Copyright © 2020 Guagua. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
